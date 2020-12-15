c2085e3c4533d688c5b4b0745963ee2cb500898b
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-12-15 14:31:14 +0000

    add bingmaps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactBingmaps } from 'react-bingmaps';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ReactBingmaps 
+        bingmapKey = "AqTBoR7ee9aqB1014xgKQ-3EXp-kv8r3InQ2OcxxxuU81xMHybJkxfsih687H2KC"
+        center={[51.5074, 0.1278]}
+        mapTypeId = {"ordnanceSurvey"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </ReactBingmaps>
   );
 }
 
