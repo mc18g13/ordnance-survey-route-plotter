deaf55beaae200596e9a61aa408190b2d8dadf79
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-12-17 12:07:57 +0000

    make route info a functional component

--- src/RouteInfo.js
+++ src/RouteInfo.js
@@ -6,28 +6,19 @@ import {
     Flex
 } from "@chakra-ui/react"
 
-import { Component } from "react";
-
-class RouteInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <Flex margin="10px" pos="fixed" background="white" w="96%" zIndex={2} top="80%" borderRadius="lg">
-                    <StatGroup px="2" >
-                    <Stat>
-                        <StatLabel>Distance</StatLabel>
-                        <StatNumber>{this.props.routeLength}km</StatNumber>
-                    </Stat>
-                    </StatGroup>
-                </Flex>
-            </div>
-        );
-    }
-
+const RouteInfo = (props) => {
+    return (
+        <div>
+            <Flex margin="10px" pos="fixed" background="white" w="96%" zIndex={2} top="80%" borderRadius="lg">
+                <StatGroup px="2" >
+                <Stat>
+                    <StatLabel>Distance</StatLabel>
+                    <StatNumber>{props.routeLength}km</StatNumber>
+                </Stat>
+                </StatGroup>
+            </Flex>
+        </div>
+    );
 }
 
 export default RouteInfo;
