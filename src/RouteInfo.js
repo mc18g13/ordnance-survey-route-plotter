348879ce480ca9a68e6d3cb4ea3f32af03278693
mc18g13 <matthewcaswell@hotmail.co.uk>
2020-12-17 13:06:53 +0000

    shrink route distance box

--- src/RouteInfo.js
+++ src/RouteInfo.js
@@ -8,16 +8,14 @@ import {
 
 const RouteInfo = (props) => {
     return (
-        <div>
-            <Flex margin="10px" pos="fixed" background="white" w="96%" zIndex={2} top="80%" borderRadius="lg">
-                <StatGroup px="2" >
-                <Stat>
-                    <StatLabel>Distance</StatLabel>
-                    <StatNumber>{props.routeLength}km</StatNumber>
-                </Stat>
-                </StatGroup>
-            </Flex>
-        </div>
+        <Flex margin="10px" pos="fixed" background="white" top="80%" borderRadius="lg">
+            <StatGroup px="2" >
+            <Stat>
+                <StatLabel>Distance</StatLabel>
+                <StatNumber>{props.routeLength}km</StatNumber>
+            </Stat>
+            </StatGroup>
+        </Flex>
     );
 }
 
